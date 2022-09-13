8de70b2aee50123e1acc5be1080d21f85beba8e2
apmfree78 <amit@profitswami.com>
2022-09-13 14:35:26 -0400

    setup local-api to direct launch react app build

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,8 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+// import { createProxyMiddleware } from 'http-proxy-middleware';
 const serve = (port, filename, dir) => {
     const app = (0, express_1.default)();
+    const packagePath = require.resolve('local-client/build/index.html');
+    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    // setting up proxy to catch requests going to react app
+    // app.use(
+    //   createProxyMiddleware({
+    //     target: 'http://localhost:3000/',
+    //     ws: true,
+    //   })
+    // );
     return new Promise((resolve, reject) => {
         app.listen(port, resolve).on('error', reject);
     });
