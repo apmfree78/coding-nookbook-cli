c53ae74b5e80bfb44d35f28f462146364290ea1f
apmfree78 <amit@profitswami.com>
2022-09-14 15:33:49 -0400

    created redux middleware to handle saving cells when specific actions are dispatch that change the cells

--- packages/local-client/src/state/middlewares/presist-middleware.ts
+++ packages/local-client/src/state/middlewares/presist-middleware.ts
@@ -1,14 +1,31 @@
-import { Dispatch } from 'react';
+import { Dispatch } from 'redux';
+import { saveCells } from '../action-creators';
+import { ActionType } from '../action-types';
 import { Action } from '../actions';
+import { RootState } from '../reducers';
 
 export const persistMiddleware = ({
   dispatch,
+  getState,
 }: {
   dispatch: Dispatch<Action>;
+  getState: () => RootState;
 }) => {
   return (next: (action: Action) => void) => {
     return (action: Action) => {
       next(action);
+
+      if (
+        [
+          ActionType.MOVE_CELL,
+          ActionType.UPDATE_CELL,
+          ActionType.INSERT_CELL_AFTER,
+          ActionType.DELETE_CELL,
+        ].includes(action.type)
+      ) {
+        console.log('save cells to file');
+        saveCells()(dispatch, getState);
+      }
     };
   };
 };
